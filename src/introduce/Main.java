d8ab150cd4b0a70b41f7d6d7cdca5ba60c044b03
coma-dwc <tamagomapon@gmail.com>
2023-08-21 13:45:04 +0900

    first commit イントロダクション 1バイト

---
+++ src/introduce/Main.java
@@ -0,0 +1,10 @@
+package introduce;
+
+public class Main {
+	public static void main(String[] args) {
+		//1バイト=8ビット 1ビット:0か1の状態を表現 1バイトでは何種類の状態を表現できるか
+		//2の8乗を計算
+		//Javaでべき乗を計算 Math.powを使用
+		System.out.println(Math.pow(2, 8));
+	}
+}
