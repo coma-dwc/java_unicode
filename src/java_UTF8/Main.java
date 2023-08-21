95772fd71d9262d5fe652d29ad148d5ffda2edb5
coma-dwc <tamagomapon@gmail.com>
2023-08-21 16:19:26 +0900

    コードポイントからUTF-8への変換

--- src/java_UTF8/Main.java
+++ src/java_UTF8/Main.java
@@ -1,5 +1,7 @@
 package java_UTF8;
 
+import java.io.ByteArrayOutputStream;
+
 //UTF-16バイト列の出力
 //数値を16進数で出力するにはInteger.toHexStringを使用
 //byte型(符号あり8bit)をそのままint型に変換すると0x80以上の数値は負数と判定されてしまうので
@@ -26,19 +28,71 @@ public class Main {
 //		printBytes(str.getBytes("UTF-8"));
 //	}
 	
+	//US-ASCII
+	
+//	public static void main(String[] args) throws Exception {
+//		String[] strs = {
+//				" !\"#$%&'()*+,-./",
+//				"0123456789:;<=>?",
+//				"@ABCDEFGHIJKLMKO",
+//				"PQRSTUYWXYZ[\\]^_",
+//				"abcdefghijklmno",
+//				"pqrstuvwxyz{|}~"
+//		};
+//		for(int i=0; i<strs.length; i++) {
+//			System.out.println(strs[i]);
+//			printBytes(strs[i].getBytes("UTF-8"));
+//		}
+//	}
+	
+	
+	//コードポイントからUTF-8への変換
+	private static byte[] toUTF8(String str) {
+		ByteArrayOutputStream buf = new ByteArrayOutputStream();
+		for(int i=0; i<str.length(); i++) {
+			int codePoint = str.codePointAt(i);
+			//サロゲートペアの場合はインデックスを進める
+			if(codePoint > 0xFFFF) {
+				i++;
+			}
+			if(codePoint <= 0x7F) {
+				//1バイトとなる文字
+				buf.write((byte)codePoint);
+			} else if(codePoint <= 0x7FF) {
+				//2バイトとなる文字
+				byte b1 = (byte)((codePoint >> 6) & 0b00011111 | 0b11000000);
+				byte b2 = (byte)(codePoint & 0b00111111 | 0b10000000);
+				buf.write(b1);
+				buf.write(b2);
+			} else if (codePoint <= 0xFFFF) {
+				//3バイトとなる文字
+				byte b1 = (byte)((codePoint >> 12) & 0b00001111 | 0b11100000);
+				byte b2 = (byte)((codePoint >> 6) & 0b00111111 | 0b10000000);;
+				byte b3 = (byte)(codePoint & 0b00111111 | 0b10000000);;
+				buf.write(b1);
+				buf.write(b2);
+				buf.write(b3);
+			} else if (codePoint <= 0x1FFFFF) {
+				//4バイトとなる文字
+				byte b1 = (byte)((codePoint >> 18) & 0b00000111 | 0b11110000);
+				byte b2 = (byte)((codePoint >> 12) & 0b00111111 | 0b10000000);
+				byte b3 = (byte)((codePoint >> 6) & 0b00111111 | 0b10000000);
+				byte b4 = (byte)(codePoint & 0b00111111 | 0b10000000 );
+				buf.write(b1);
+				buf.write(b2);
+				buf.write(b3);
+				buf.write(b4);
+			}
+		}
+		return buf.toByteArray();
+	}
 	
 	public static void main(String[] args) throws Exception {
-		String[] strs = {
-				" !\"#$%&'()*+,-./",
-				"0123456789:;<=>?",
-				"@ABCDEFGHIJKLMKO",
-				"PQRSTUYWXYZ[\\]^_",
-				"abcdefghijklmno",
-				"pqrstuvwxyz{|}~"
-		};
-		for(int i=0; i<strs.length; i++) {
-			System.out.println(strs[i]);
-			printBytes(strs[i].getBytes("UTF-8"));
-		}
+		String str = "Hello 世界と𩸽!";
+		System.out.println(str);
+		printBytes(str.getBytes("UTF-8"));
+		printBytes(toUTF8(str));
 	}
 }
+
+//文字のコードポイントを取得するにはcodePointAtメソッドを使用する
