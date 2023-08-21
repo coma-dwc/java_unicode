0065785dcd94c12a8ef711d6cf744352f69cb720
coma-dwc <tamagomapon@gmail.com>
2023-08-21 13:47:52 +0900

    2バイト

--- src/introduce/Main.java
+++ src/introduce/Main.java
@@ -5,6 +5,11 @@ public class Main {
 		//1バイト=8ビット 1ビット:0か1の状態を表現 1バイトでは何種類の状態を表現できるか
 		//2の8乗を計算
 		//Javaでべき乗を計算 Math.powを使用
-		System.out.println(Math.pow(2, 8));
+//		System.out.println(Math.pow(2, 8));
+		
+		
+		//2バイト(16ビット)
+		//2の16乗を計算
+		System.out.println(Math.pow(2, 16));
 	}
 }
