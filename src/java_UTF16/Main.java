ceb6fd5f7bcc20b859a5eaaa6c947552577894eb
coma-dwc <tamagomapon@gmail.com>
2023-08-21 14:29:40 +0900

    コードポイントを表示(2)

--- src/java_UTF16/Main.java
+++ src/java_UTF16/Main.java
@@ -7,7 +7,9 @@ public class Main {
 		//String型で表される文字列の各位置文字はchar型のデータ
 		
 		//String文字列を一文字ずつ改行区切りで出力するプログラムを完成
-		String str = "Hello 世界!";
+//		String str = "Hello 世界!";
+		//65536文字以上の文字が表現できるか確認(𩸽)
+		String str = "Hello 世界と𩸽!";
 		for(int i=0; i<str.length(); i++) {
 			char c = str.charAt(i);
 			//コードポイントを表示
